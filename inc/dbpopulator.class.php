d6c167a7334a7abb21ee459ec4279fc0b5997376
Antoine Lemarchand <antoine@itsm-ng.home>
2023-09-12 16:09:51 +0200

    feat(dbpopulator.class.php): simplified functions based on current features

--- inc/dbpopulator.class.php
+++ inc/dbpopulator.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ---------------------------------------------------------------------
  * ITSM-NG
@@ -50,59 +51,12 @@ class PluginDbpopulatorDbpopulator extends CommonDBTM
     function populate(string $format, string $table, int $quantity): void
     {
         global $DB;
-        $columns = self::getColumnsFromTable($table);
-        $query = "INSERT INTO " . $table . " (";
-        foreach ($columns as $index => $value) {
-            if ($index == 'name') {
-                $query .= $index . ",";
-            }
-        }
-        $query = substr($query, 0, -1);
-        $query .= ") VALUES ";
+        $query = "INSERT INTO " . $table . " (name) VALUES ";
         foreach (range(1, $quantity) as $number) {
-            $query .= "(";
-            foreach ($columns as $index => $value) {
-                if ($index == "name") {
-                    $query .= '"' .self::generateFormatedValue($format). '",';            
-                }
-            }
-            $query = substr($query, 0, -1);
-            $query .= "),";
+            $query .= "('" . self::generateFormatedValue($format) . "'),";
         }
         $query = substr($query, 0, -1);
         $DB->query($query);
-    }
-
-    /**
-     * get the columns of a table
-     * 
-     * @param string $table
-     * @global object $DB
-     * 
-     * @return array
-     */
-    private function getColumnsFromTable(string $table)
-    {
-        global $DB;
-        $queryValue = "select GROUP_CONCAT(column_name) nonnull_columns 
-        from information_schema.columns 
-        where table_schema = '".$DB->dbdefault."' and
-        table_name = '" . $table . "' and
-        is_nullable = 'YES'";
-        $values = explode(',', $DB->query($queryValue)->fetch_assoc()['nonnull_columns']);
-        $queryValue = "select GROUP_CONCAT(column_type) nonnull_columns 
-        from information_schema.columns 
-        where table_schema = '".$DB->dbdefault."' and
-        table_name = '" . $table . "' and
-        is_nullable = 'YES'";
-        $types = explode(',', $DB->query($queryValue)->fetch_assoc()['nonnull_columns']);
-
-        $ret = [];
-        foreach ($values as $index => $col) {
-            $ret[$col] = $types[$index];
-        }
-
-        return $ret;
     }
 
     /**
@@ -111,11 +65,12 @@ class PluginDbpopulatorDbpopulator extends CommonDBTM
      * @param string $format
      * @return string
      */
-    private function generateFormatedValue(string $format): string {
+    private function generateFormatedValue(string $format): string
+    {
         $faker = Faker\Factory::create();
         $random_value = $faker->randomNumber(6);
         if (count(explode("%%", $format)) == 1) {
-            return $format.$random_value;
+            return $format . $random_value;
         } else {
             return str_replace("%%", $random_value, $format);
         }
